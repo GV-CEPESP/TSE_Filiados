27a01145e49c3c1ae1e978bf206f453882f21628
Rafael Coelho <rafael.coelho.x@gmail.com>
2018-06-12 14:23:31 -0400

    testeasd

--- script_clean.R
+++ script_clean.R
@@ -1,3 +1,13 @@
 library(tidyverse)
 
-filiados <- read_csv("filiados.csv", n_max = 1000)
+filiados <- read_rds("filiados_test.rds")
+
+
+# 1. Pré-Processamento ----------------------------------------------------
+
+## 1.1. Renomenado as variáveis
+## 
+names(filiados) <- c("DATA_EXTRACAO", "HORA_EXTRACAO", "NUMERO_INSCRICAO", "NOME_FILIADO", "SIGLA_PARTIDO", "NOME_PARTIDO",
+                     "UF", "CODIGO_MUNICIPIO", "NOME_MUNICIPIO", "ZONA_ELEITORAL", "SECAO_ELEITORAL", "DATA_FILIACAO",
+                     "SIT_REGISTRO", "TIPO_REGISTRO", "DATA_PROCESSAMENTO", "DATA_DESFILIACAO", "DATA_CANCELAMENTO",
+                     "DATA_REGULARIZACAO", "MOTIVO_CANCELAMENTO")
