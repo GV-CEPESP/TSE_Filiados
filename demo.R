e5e95be8ce27c0f0ab98108f36f6cb4367bc1d6a
Rafael Coelho <RCCSilva@users.noreply.github.com>
2018-06-20 11:44:07 -0300

    del

--- demo.R
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-
-data <- read_rds("filiados.rds")
-
-glimpse(data)
-
-data %>% 
-  mutate(`NUMERO DA INSCRICAO` == as.numeric(`NUMERO DA INSCRICAO`)) %>% 
-  filter(`NUMERO DA INSCRICAO` == 9288940752)
