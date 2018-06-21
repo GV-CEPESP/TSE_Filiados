6b8ffc4e34f4d6e88930899580fb663c2d122ff1
Rafael Coelho <RCCSilva@users.noreply.github.com>
2018-06-21 13:54:08 -0300

    começo das funções

--- script_clean.R
+++ script_clean.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-filiados <- read_rds("filiados_test.rds")
+filiados <- read_rds("filiados.rds")
 
 # 1. Pré-Processamento ----------------------------------------------------
 
@@ -11,6 +11,8 @@ names(filiados) <- c("DATA_EXTRACAO", "HORA_EXTRACAO", "NUMERO_INSCRICAO", "NOME
                      "SIT_REGISTRO", "TIPO_REGISTRO", "DATA_PROCESSAMENTO", "DATA_DESFILIACAO", "DATA_CANCELAMENTO",
                      "DATA_REGULARIZACAO", "MOTIVO_CANCELAMENTO")
 
+glimpse(filiados)
+
 ## 1.2. Transformação das Variáveis 
 
 filiados <- filiados %>% 
@@ -18,7 +20,8 @@ filiados <- filiados %>%
          DATA_SAIDA   = case_when(!is.na(DATA_DESFILIACAO) ~ DATA_DESFILIACAO,
                                   !is.na(DATA_CANCELAMENTO)~ DATA_CANCELAMENTO,
                                   T                        ~ "19/07/2018"),
-         DATA_SAIDA   = parse_date(DATA_SAIDA, format = "%d/%m/%Y"))
+         DATA_SAIDA   = parse_date(DATA_SAIDA, format = "%d/%m/%Y"),
+         NUMERO_INSCRICAO_INT = as.numeric(NUMERO_INSCRICAO))
 
 filiados <- filiados %>% 
   select(NUMERO_INSCRICAO, NOME_FILIADO, SIGLA_PARTIDO, NOME_PARTIDO, UF, CODIGO_MUNICIPIO, NOME_MUNICIPIO, DATA_ENTRADA, DATA_SAIDA)
@@ -26,6 +29,23 @@ filiados <- filiados %>%
 ## 1.3. Consistência
 
 ### 1.3.1. Teste de Repetições
+
+filiados %>% 
+  count(NUMERO_INSCRICAO)
+
+NUM <- filiados$NUMERO_INSCRICAO_INT
+
+NUM <- unique(NUM)
+
+overlap_detect <- function(entrada1,saida1,entrada2,saida2){
+  # Essa função testa se dois pares de datas de entrada e saída possuem
+  # uma sobreposição.
+}
+  
+overlap_count <- function(){
+  # Essa função faz uma contagem de sobreposições de datas de filiação para
+  # cada numero de inscrição
+}
 
 # Nesta seção, o objetivo é testar a existência de mais de uma filiação para uma pessoa
 # em um mesmo período de tempo.
